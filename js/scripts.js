a78568bdf3c0d70c90b9772bb312cdcba8d6021f
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-06-21 09:18:09 +0300

    adds the js code to show and hide the what we do description. also changes the classes for description to ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 // js code to reveal what we do on click
-$(document).ready(function(){
-
-    
-
-
+// design toggle
+$(document).ready(function(){   
+    $("#design-icon").click(function(){
+      $("#design-icon").hide();
+      $("#design-description").show();
+    });
+    $("#design-description").click(function(){
+      $("#design-description").hide();
+      $("#design-icon").show();
+    });
 })
+// development toggle
+$(document).ready(function(){   
+    $("#dev-icon").click(function(){
+      $("#dev-icon").hide();
+      $("#development-description").show();
+    });
+    $("#development-description").click(function(){
+      $("#development-description").hide();
+      $("#dev-icon").show();
+    });
+})
+    //   product toggle
+    $(document).ready(function(){   
+      $("#product-icon").click(function(){
+        $("#product-icon").hide();
+        $("#product-description").show();
+      });
+      $("#product-description").click(function(){
+        $("#product-description").hide();
+        $("#product-icon").show();
+      
+    });
+    
+    });
